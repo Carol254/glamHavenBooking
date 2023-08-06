77638a656ad3af4c55e025589c8a47901ebe965b
Caroline <wanguic49@gmail.com>
2023-08-06 23:08:08 +0300

    few changes made

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+interface Service {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
+
 export class HomeComponent {
+  services: Service[] = [
+    {value: 's-1', viewValue: 'Manicure'},
+    {value: 's-2', viewValue: 'Pedicure'},
+    {value: 's-3', viewValue: 'Facial'},
+    {value: 's-4', viewValue: 'Hair Styling'},
+    {value: 's-3', viewValue: 'Sea C Spa Treatment'}
+  ];
+
 
 }
